2490102111d68b4165a40fad969cadee3d43e272
wangminghao <12345678>
2023-08-02 09:40:22 +0800

    24 两两交换链表中的元素

---
+++ leetcode/24.go
@@ -0,0 +1,19 @@
+package leetcode
+
+/*
+*
+两两交换链表中的元素
+*/
+func swapPairs(head *ListNode) *ListNode {
+	dummyHead := &ListNode{0, head}
+	temp := dummyHead
+	for temp.Next != nil && temp.Next.Next != nil {
+		node1 := temp.Next
+		node2 := temp.Next.Next
+		temp.Next = node2
+		node1.Next = node2.Next
+		node2.Next = node1
+		temp = node1
+	}
+	return dummyHead.Next
+}
