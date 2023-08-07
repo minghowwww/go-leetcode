28d3277f65a83c1ad60e01ba7d2aceba51bbe2a1
wangminghao <12345678>
2023-08-07 09:53:58 +0800

    242 增加排序解法

--- leetcode/242.go
+++ leetcode/242.go
@@ -1,32 +1,53 @@
 package leetcode
 
+import "sort"
+
 /*
 *
 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
 
 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
 */
+/**
+排序解法
+*/
 func isAnagram(s string, t string) bool {
-	hash := [26]int{}
-	// 初始化hash表
-	for i := 0; i < len(hash); i++ {
-		hash[i] = 0
-	}
-	// s 存储 往里加
-	for i := 0; i < len(s); i++ {
-		index := byte(s[i]) - byte('a')
-		hash[index]++
-	}
-	// t 存储 往下减
-	for i := 0; i < len(t); i++ {
-		index := byte(t[i]) - byte('a')
-		hash[index]--
-	}
-	// 判断hash表中是否都为0
-	for i := 0; i < len(hash); i++ {
-		if hash[i] != 0 {
-			return false
-		}
-	}
-	return true
+	ss := []rune(s)
+	sort.Slice(ss, func(i, j int) bool {
+		return byte(ss[i]) < byte(ss[j])
+	})
+	tt := []rune(t)
+	sort.Slice(tt, func(i, j int) bool {
+		return byte(tt[i]) < byte(tt[j])
+	})
+	return string(ss) == string(tt)
 }
+
+/*
+*
+hash表解法
+*/
+//func isAnagram(s string, t string) bool {
+//	hash := [26]int{}
+//	// 初始化hash表
+//	for i := 0; i < len(hash); i++ {
+//		hash[i] = 0
+//	}
+//	// s 存储 往里加
+//	for i := 0; i < len(s); i++ {
+//		index := byte(s[i]) - byte('a')
+//		hash[index]++
+//	}
+//	// t 存储 往下减
+//	for i := 0; i < len(t); i++ {
+//		index := byte(t[i]) - byte('a')
+//		hash[index]--
+//	}
+//	// 判断hash表中是否都为0
+//	for i := 0; i < len(hash); i++ {
+//		if hash[i] != 0 {
+//			return false
+//		}
+//	}
+//	return true
+//}
